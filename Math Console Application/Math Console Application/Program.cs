760cd739b8a5b37114d35a1db9a9d0d80ed1b6b1
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-05-18 19:01:44 -0700

    Math Console Application

---
+++ Math Console Application/Math Console Application/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int product = 5000001 * 50;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int total = 250 + 25;
+            //Console.WriteLine("TwoHundredFifty plus Twentyfive = " + total.ToString());
+            //Console.ReadLine();
+
+            //int num1 = 25;
+            //float num2 = 12.5f;
+            //float total = num1 - num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 250 > 50;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int remainder = 250 % 7;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+        }
+    }
+}
