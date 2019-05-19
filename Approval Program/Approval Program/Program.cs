6fe9b6d99f9b69122adc20f90cc8f8ea5c049956
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-05-19 16:08:06 -0700

    Boolean Logic Exercise

---
+++ Approval Program/Approval Program/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Approval_Program
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+
+
+            //            REQUIREMENTS
+
+            //Ask the following questions:
+
+            //            What is your age ?
+
+            //            Have you ever had a DUI?
+
+            //How many speeding tickets do you have?
+
+            //Use the following qualification rules to determine if the applicant qualifies for car insurance:
+
+            //Applicants must be over 15 years old.
+
+            //Applicants must not have any DUI’s.
+
+            //Applicants must not have more than 3 speeding tickets.
+
+            //Print the result of the boolean expression created from the above business rules.
+
+            Console.WriteLine("What is your age?");
+            Console.WriteLine(30);
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine("false");
+            Console.WriteLine(" How many speeding tickets do you have?");
+            Console.WriteLine(1);
+            
+
+            int AppAge = 30;
+            string AppDUI = "0";
+            int SpeedingTickets = 1;
+            bool isQualified = (AppAge > 15 && AppDUI == "0" && SpeedingTickets < 3);
+
+            Console.WriteLine(isQualified);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Boolean Logic/Boolean Logic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && true && true || false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+
+            Console.ReadLine();
+        }
+    }
+}
