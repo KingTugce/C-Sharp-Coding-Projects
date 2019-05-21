5f0064fa2193ab2962d86b577fb28218ebf862f9
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-05-21 16:32:39 -0700

    sync

---
+++ String Functions/String Functions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //string clouds = "Apricot said \" Hi Clouds\" ! \t\n Three different clouds type: \n\t \n High clouds are cirrus, cirrostratus, and cirrocumulus. \n Mid-level clouds are altostratus and altocumulus. \n Low-level clouds are cumulus congestus or towering cumulus.";
+
+            //string fruit = "Peach";
+            //fruit = "Nectarine";
+
+            //clouds = clouds.ToUpper();
+
+
+            //Console.WriteLine(clouds);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Apricot said \" Hi Clouds\" ! ");
+            System.Threading.Thread.Sleep(500);
+            sb.Append("\t\n Three different clouds type: \n\t");
+            sb.Append("\n High clouds are cirrus, cirrostratus, and cirrocumulus.");
+            sb.Append("\n Mid-level clouds are altostratus and altocumulus.");
+            sb.Append("\n Low-level clouds are cumulus congestus or towering cumulus.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ String/String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Tugce";
+            //string quote = "The man said, \"Hello\" , Tugce. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C:\Users\Jesse";
+
+
+            //bool trueOrFalse = name.Contains("c");
+            //trueOrFalse = name.EndsWith("c");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+
+
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            //string name = "Tugce";
+            //name = "Joe";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Tugce");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
