d506f7b8bed9c9c21acefe411e28538455d9ff1d
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-05-21 00:10:48 -0700

    sync

--- Math Console Application/Math Console Application/Program.cs
+++ Math Console Application/Math Console Application/Program.cs
@@ -10,6 +10,37 @@ namespace MathAndComparisonOperations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter any number you want to be multiplied by 50. ");
+            double userInput1 = Double.Parse(Console.ReadLine());
+            double product1 = userInput1 * 50;
+            Console.WriteLine("The answer is " + product1);
+            System.Threading.Thread.Sleep(900);
+
+            Console.WriteLine("Enter any number you want to be added by 25. ");
+            double userInput2 = Double.Parse(Console.ReadLine());
+            double product2 = userInput2 + 25;
+            Console.WriteLine("The answer is " + product2);
+            System.Threading.Thread.Sleep(900);
+
+            Console.WriteLine("Enter any number you want to be divided by 12.5. ");
+            double userInput3 = Double.Parse(Console.ReadLine());
+            double product3 = userInput3 / 12.5;
+            Console.WriteLine("The answer is " + product3);
+            System.Threading.Thread.Sleep(900);
+
+            Console.WriteLine("Enter any number you want to be greater than 50. ");
+            double userInput4 = Double.Parse(Console.ReadLine());
+            bool trueOrFalse = userInput4 > 50;
+            Console.WriteLine("The answer is " + trueOrFalse.ToString());
+            System.Threading.Thread.Sleep(900);
+
+            Console.WriteLine("Enter any number you want to be divided it by 7, and printed the remainder. ");
+            double userInput5 = Double.Parse(Console.ReadLine());
+            double remainder = userInput5 % 7;
+            Console.WriteLine("The answer is " + remainder);
+            System.Threading.Thread.Sleep(900);
+
+
             //int product = 5000001 * 50;
             //Console.WriteLine(product);
             //Console.ReadLine();
