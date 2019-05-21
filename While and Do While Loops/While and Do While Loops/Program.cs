859efba1c846a9fcb4ae799fd13bbfaffc90f8f0
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-05-21 01:16:41 -0700

    While and Do Loops

---
+++ While and Do While Loops/While and Do While Loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_and_Do_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+
+        //WHILE LOOP
+        {
+            int index = 500;
+            while (index <= 525)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.ReadLine();
+        }
+    
+        //DO WHILE LOOP
+        //  {
+        //    int index = 530;
+        //    do
+        //    {
+        //        Console.WriteLine(index);
+        //        index++;
+        //    } while (index <= 525) ;
+
+        //        Console.ReadLine();
+        //  }
+
+        
+    }
+}
