6608f6df61f5b86722a87266804ea2821f8c0a1d
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-05-22 17:16:24 -0700

    sync changes

---
+++ Iterations Exercise/Iterations Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations_Exercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           //List<string> Clouds = new List<string>();
+           // Clouds[0] = "Cumulus humilis";
+           // Clouds[1] = "Cumulus mediocris";
+           // Clouds[2] = "Cumulonimbus";
+           // Clouds[3] = "Sirrus";
+           // Clouds[4] = "Sirrostratus";
+
+
+
+            string[] Clouds = { "Cumulus humilis ", "Cumulus mediocris ", "Cumulonimbus ", "Sirrus ", "Sirrostratus " };
+
+            Console.WriteLine("What is your favorite cloud type?:\n");
+            string favcloud = Console.ReadLine();
+
+             for (int i = 0; i < Clouds.Length; i++)
+                {
+                     Clouds[i] = Clouds[i] + favcloud;
+
+                     Console.WriteLine(Clouds[i]);
+                }
+            Console.ReadLine();
+
+        }
+
+    }
+}
