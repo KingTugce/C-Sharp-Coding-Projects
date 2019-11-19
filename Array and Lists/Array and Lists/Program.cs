66a5d6a220a56f9d3edcef30b361624caafa9592
KingTugce <43044091+KingTugce@users.noreply.github.com>
2019-11-18 21:08:22 -0800

    Update Program.cs
    there is still work to do

--- Array and Lists/Array and Lists/Program.cs
+++ Array and Lists/Array and Lists/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create an array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
+2. Create an array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
+3. Add in a message that displays when the user selects an index that doesn’t exist.
+4. Create a List of strings. Ask the user to select an index of the List and then display the content at that index on the screen.
+By:Tugce Simsek
+ */
+using System;
 using System.Collections.Generic;
 
 namespace Array_and_Lists
