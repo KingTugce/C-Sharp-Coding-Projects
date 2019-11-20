2b80cfcf0530ac349a4a05a038471b6816057889
KingTugce <43044091+KingTugce@users.noreply.github.com>
2019-11-19 18:52:49 -0800

    Update notes for Boolean Logic

--- Boolean Logic/Boolean Logic/Program.cs
+++ Boolean Logic/Boolean Logic/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Boolean_Logic
 {
@@ -10,15 +6,15 @@ namespace Boolean_Logic
     {
         static void Main(string[] args)
         {
-          //   N.B: Items can be only true or false
+          //N.B: Items can be only true or false
             
-            //nb: with && operator all have to be true.
+            //N.B: with && operator all have to be true.
             //Console.WriteLine(true && true && true || false);
             //Console.WriteLine(true && true);
             //Console.WriteLine(true && false);
             //Console.WriteLine(false && false);
 
-           //nb: one of them is true is true
+           //N.B: one of them is true is true
             //Console.WriteLine(true || true);
             //Console.WriteLine(true || false);
             //Console.WriteLine(false || false);
@@ -30,8 +26,10 @@ namespace Boolean_Logic
             //Console.WriteLine(true != true);
             //Console.WriteLine(true != false);
             //Console.WriteLine(false != false);
-
-            //nb: if one of this are true but not both.(x OR OPERATOR)
+            
+            //(X OR OPERATOR)
+            //N.B: if one of this are true but not both.
+            //N.B: if one of this are false  but not both.
             //Console.WriteLine(true ^ true);
             //Console.WriteLine(true ^ false);
             //Console.WriteLine(false ^ false);
