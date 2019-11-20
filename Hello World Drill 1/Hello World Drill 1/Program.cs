6424ecf18f2f5dcab7282f976626a7a4977b5d03
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-11-20 13:32:23 -0800

    Hello World (basic program) Drill 1

---
+++ Hello World Drill 1/Hello World Drill 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Hello_World_Drill_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+        }
+    }
+}
