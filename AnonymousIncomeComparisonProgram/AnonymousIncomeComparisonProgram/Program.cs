4b75e394ac0d8507290ea07e7c508de9c8a808a9
KingTugce <43044091+KingTugce@users.noreply.github.com>
2019-11-20 19:09:32 -0800

    Math and Comparison Drill 5

--- AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram/Program.cs
+++ AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram/Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*EXERCISE: MATH AND COMPARISON OPERATORS
+Your job is to create an anonymous income comparison program.
+REQUIREMENTS
+•	The program must start by printing “Anonymous Income Comparison Program” to the screen.
+•	It must then print “Person 1” to the screen and get the following details:
+Hourly Rate
+Hours worked per week
+•	It must then print “Person 2” to the screen and then get the following details:
+Hourly rate
+Hours worked per week
+•	It must then print to the screen “Annual salary of Person 1:” and write the exact salary below it.
+•	It must then print to the screen “Annual salary of Person 2:” and write the exact salary below it.
+•	It must then print to the screen “Does Person 1 make more money than Person 2?” and write the true or false value of this statement below it.
 
+EXAMPLE
+Anonymous Income Comparison Program
+Person 1
+Hourly Rate?
+15
+Hours worked per week?
+40
+Person 2
+Hourly Rate?
+20
+Hours worked per week?
+40
+Weekly salary of Person 1:
+600
+Weekly salary of Person 2:
+800
+Does Person 1 make more money than Person 2?
+false
+*/
 namespace AnonymousIncomeComparisonProgram
 {
     class Program
