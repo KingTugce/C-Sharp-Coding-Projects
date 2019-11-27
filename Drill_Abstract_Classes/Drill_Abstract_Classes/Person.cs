233a14f22135019d1419af30e6aa97a5749d4db8
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-11-26 21:49:38 -0800

    Update Abstract Classes Drill

---
+++ Drill_Abstract_Classes/Drill_Abstract_Classes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Abstract_Classes
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void ID()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Drill_Abstract_Classes/Drill_Abstract_Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create an abstract class called Person with two properties: string firstName and string lastName.
+2. Give it the method SayName().
+3. Create another class called Employee and have it inherit from the Person class.
+4. Implement the SayName() method inside of the Employee class.
+5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+*/
+namespace Drill_Abstract_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Tasslehof";
+            employee.lastName = "Barfoot";
+            employee.ID();
+        }
+    }
+}
