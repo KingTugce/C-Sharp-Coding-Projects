18e07fcc2340bd03c20ea5c7c9875268888771fc
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-11-27 01:09:39 -0800

    Update Struct Drill

--- Enums_Drill/Enums_Drill/Program.cs
+++ Enums_Drill/Enums_Drill/Program.cs
@@ -9,7 +9,10 @@ IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
 2. Prompt the user to enter the current day of the week.
 3. Assign the value to a variable of that enum data type you just created.
 4. Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week." to the console if an error occurs.
-Note - In the last video, we explained the underlying concepts behinds enums.Nobody knows everything about a programming language and its features, so it's our job as programmers to do research on how to work with various data types, classes and libraries. For this drill, you'll need to do some additional research on how to parse enums.
+Note - In the last video, we explained the underlying concepts behinds enums.
+Nobody knows everything about a programming language and its features, 
+so it's our job as programmers to do research on how to work with various data types, classes and libraries. 
+For this drill, you'll need to do some additional research on how to parse enums.
 */
 namespace Enums_Drill
 {
---
+++ Struct_Drill/Struct_Drill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+2. In the Main() method, create an object of data type Number and assign an amount to it.
+3. Print this amount to the console.
+*/
+namespace Struct_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Number num = new Number();
+                num.Amount = 115.50m;
+                Console.WriteLine(num.Amount);
+                Console.ReadLine();
+            
+        }
+
+        struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+
+}
