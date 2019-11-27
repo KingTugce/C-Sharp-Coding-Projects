33b416a67470b0382b66769e0c97a1b03b1d7132
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-11-27 02:40:09 -0800

    Update Time and Date in the Future Drill

---
+++ Time And Date Drill/Time And Date Drill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Prints the current date and time to the console.
+2. Asks the user for a number.
+3. Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+*/
+namespace Time_And_Date_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine(DateTime.Now);
+            
+            Console.Write("Hi there, Please Enter a number and let me show you what time will be in the future from now: ");
+            Double userInput = Convert.ToDouble(Console.ReadLine());
+
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.AddHours(userInput));
+            Console.ReadLine();
+        }
+    }
+}
