bef0e1c19cb6666b78d40ce8c47e0b82adf82938
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-11-26 22:30:07 -0800

    Update Interface Drill

---
+++ Drill_Interface/Drill_Interface/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Interface
+{
+    class Employee : Person, IQuittable
+    {
+        public void ID()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Employee resigned: ");
+            ID();
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Drill_Interface/Drill_Interface/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Interface
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void ID()
+        {
+            Console.WriteLine("Name is : {0} {1}" + FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+
+    }
+}
