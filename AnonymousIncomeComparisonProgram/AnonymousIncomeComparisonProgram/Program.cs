1f53ca600af5112559d48f0357c3884e8048ed66
tgcsmsk <43044091+tgcsmsk@users.noreply.github.com>
2019-11-28 01:24:04 -0800

    Update Income Comparison Drill

--- AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram/Program.cs
+++ AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram/Program.cs
@@ -38,38 +38,34 @@ namespace AnonymousIncomeComparisonProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anonymous Income Comparison Program");
-            System.Threading.Thread.Sleep(500);
-//Person1
+            Console.WriteLine("\tAnonymous Income Comparison Program");
+
+       //Person1
             Console.WriteLine("Person 1");
-            Console.WriteLine("Hourly Rate");
-            int hrRate1 = Int32.Parse(Console.ReadLine());
-
-            Console.WriteLine("Hours worked per week");
+            Console.WriteLine("Hourly Rate?");
+            int hrRate1 = Int32.Parse(Console.ReadLine()) ;
+            Console.WriteLine("Hours worked per week?");
             int hrPerWeek1 = Int32.Parse(Console.ReadLine());
-
-//Person2
-            Console.WriteLine("Person 2");
-            Console.WriteLine("Hourly Rate");
-            int hrRate2 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Hours worked per week");
-            int hrPerWeek2 = Int32.Parse(Console.ReadLine());
-            
-// Calculating their Weekly salaries
-            int weeksalary1 = hrRate1 * hrPerWeek1 ;
+            int weeksalary1 = hrRate1 * hrPerWeek1;                                                  // Calculating their Weekly salaries
             Console.WriteLine("Weekly salary for Person 1 is " + weeksalary1);
 
-            int weeksalary2 = hrRate2 * hrPerWeek2 ;
+       //Person2
+            Console.WriteLine("\nPerson 2");
+            Console.WriteLine("Hourly Rate?");
+            int hrRate2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week?");
+            int hrPerWeek2 = Int32.Parse(Console.ReadLine());
+            int weeksalary2 = hrRate2 * hrPerWeek2;                                                 // Calculating their Weekly salaries
             Console.WriteLine("Weekly salary for Person 2 is " + weeksalary2);
 
-// Comparison
-            Console.WriteLine("Does Person 1 make more money than Person 2?");
+       //Comparison   
+            Console.WriteLine("Does Person 1 make more money than Person 2?:");
             bool trueOrFalse = weeksalary1 > weeksalary2;
             Console.WriteLine(trueOrFalse.ToString());
             Console.ReadLine();
 
         }
-        
+
     }
 }
 
